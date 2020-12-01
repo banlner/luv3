aaf62ca1ccc4035dcf76960f669cc5993a71f951
banlner <71725023+banlner@users.noreply.github.com>
2020-12-01 00:37:51 +0000

    Update clearshout.js

--- commands/clearshout.js
+++ commands/clearshout.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 exports.run = async (client, message, args) => {
-    if(!message.member.roles.cache.some(role =>["Ranking Permissions", "Shout Permissions"].includes(role.name))){
+    if(!message.member.roles.cache.some(role =>["Staff"].includes(role.name))){
         return message.channel.send({embed: {
             color: 16733013,
-            description: "You need the `Ranking Permissions` or `Shout Permissions` role to run this command.",
+            description: "You need to be a London Underground Staffing member to use this command",
             author: {
                 name: message.author.tag,
                 icon_url: message.author.displayAvatarURL()
